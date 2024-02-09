db661bd339295b370bb1071bf85fc24dc516246d
JonathanJoelVos <dev.jonathan.joel.vos@gmail.com>
2024-02-09 10:31:24 -0300

    que massa

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
-export default function Home(){
-    return <div>Mica eu te amo. Se voce me ama da uma risadinha(e me manda uma foto hihi)</div>
+export default function Home() {
+    return <div>Mica eu te amo. Se voce me ama da uma risadinha(e me manda uma foto hihi) que massa. mano que parada loca</div>
 }
