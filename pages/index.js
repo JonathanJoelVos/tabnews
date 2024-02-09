75126258fb14cd26ec3743aeea1069d972f15a34
JonathanJoelVos <dev.jonathan.joel.vos@gmail.com>
2024-02-09 14:58:01 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Mica eu te amo. Se voce me ama da uma risadinha(e me manda uma foto hihi)</div>
+  return (
+    <div>
+      Mica eu te amo. Se voce me ama da uma risadinha(e me manda uma foto hihi)
+    </div>
+  );
 }
